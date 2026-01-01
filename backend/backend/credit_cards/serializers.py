e6d7348f7fcee9206f2fc97d53bfe59e49a0627d
Vaibhav159 <vlodha98@gmail.com>
2026-01-01 23:13:59 +0530

    back ref

--- backend/backend/credit_cards/serializers.py
+++ backend/backend/credit_cards/serializers.py
@@ -1,14 +1,26 @@
+from django.db.models import Q
 from rest_framework import serializers
 from .models import CreditCard
+from backend.guides.models import Guide
+
 
 class CreditCardSerializer(serializers.ModelSerializer):
     detailedGuide = serializers.CharField(source='detailed_guide')
     applyLink = serializers.URLField(source='apply_link')
+    related_guides = serializers.SerializerMethodField()
 
     class Meta:
         model = CreditCard
         fields = [
-            "id", "slug", "name", "bank", "category", "image", 
-            "verdict", "detailedGuide", "applyLink", "features", 
-            "fees", "eligibility", "rewards", "metadata", "tags"
+            "id", "slug", "name", "bank", "category", "image",
+            "verdict", "detailedGuide", "applyLink", "features",
+            "fees", "eligibility", "rewards", "metadata", "tags",
+            "related_guides"
         ]
+
+    def get_related_guides(self, obj: CreditCard):
+        all_guides = Guide.objects.filter(
+            Q(tags__contains=[obj.name]) | Q(tags__contains=[obj.slug])
+        ).values()[:5]
+
+        return all_guides
